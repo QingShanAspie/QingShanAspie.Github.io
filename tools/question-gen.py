12e823ffd2da74e756093966746a9655ced6770a
GloomyGhost <740291272@qq.com>
2019-10-20 22:53:48 +0800

    中文一下下

--- tools/question-gen.py
+++ tools/question-gen.py
@@ -1,9 +1,11 @@
 len = input("有多少个问题？：")
 filename = input("生成的文件名？：")
 
+len = int(len) + 1
+
 que = "question:"
 
-for i in range(0,int(len)):
+for i in range(0,len):
     que += "\n  - id: " + str(i) + "\n    str: \n    rev: \n"
 
 filename += ".yml"
