676248be54bc9324587942f5b1c0d19e58c1aac5
gloomyghost <740291272@qq.com>
2019-10-29 22:21:46 +0800

    all with wasm

--- src/Src/Main.cpp
+++ src/Src/Main.cpp
@@ -7,6 +7,50 @@
 using namespace std;
 using namespace emscripten;
 
+string OAQ(int result)
+{
+    if (result <= 94)
+    {
+        return "非述情障碍";
+    }
+    else if (result > 94 && result <= 112)
+    {
+        return "可能有述情障碍";
+    }
+    else if (result > 112)
+    {
+        return "述情障碍";
+    }
+    else
+    {
+        return ("请完成量表！！！");
+    }
+}
+
+string AQ_C(int result)
+{
+	if (result <= 21)
+	{
+		return ("孩子非孤独症谱系");
+	}
+	else if (result > 21 && result <= 25)
+	{
+		return ("孩子有一些孤独症谱系的特质");
+	}
+	else if (result > 25 && result <= 31)
+	{
+		return ("孩子可能有高功能孤独症谱系障碍");
+	}
+	else if (result > 31)
+	{
+		return ("孩子非常可能有高功能孤独症谱系障碍");
+	}
+	else
+	{
+		return ("请完成量表！！！");
+	}
+}
+
 string AQ_A(int result)
 {
 	if (result <= 21)
@@ -36,5 +80,7 @@ string AQ_A(int result)
 //-----------------------------------------------------------------------------
 EMSCRIPTEN_BINDINGS(QSA)
 {
+    emscripten::function("OAQ", &OAQ);
+    emscripten::function("AQ_C", &AQ_C);
 	emscripten::function("AQ_A", &AQ_A);
 }
