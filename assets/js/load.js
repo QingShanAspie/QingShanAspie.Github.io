26d0dd8190b6db864dcd55766850e042cb61a59f
GloomyGhost <740291272@qq.com>
2019-11-21 21:03:09 +0800

    speed up

--- assets/js/load.js
+++ assets/js/load.js
@@ -1,6 +1,6 @@
 function load() {
     setTimeout("loading.style.transition='opacity 0.3s'", 0);
-    setTimeout("loading.style.opacity=0", 500);
-    setTimeout("loading.style.display='none'", 800);
-    setTimeout("body.style.display = 'block' ", 600);
+    setTimeout("loading.style.opacity=0", 0);
+    setTimeout("loading.style.display='none'", 0);
+    setTimeout("body.style.display = 'block' ", 0);
 }
