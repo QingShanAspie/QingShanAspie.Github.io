bbb7fe55944206a5e97fa87bfd70e079a87cdc7e
Gloomy Ghost <740291272@qq.com>
2019-12-25 21:16:56 +0800

    Update CopyRight.js

--- assets/js/CopyRight.js
+++ assets/js/CopyRight.js
@@ -1,16 +1,20 @@
 function setClipboardText(event) {
     event.preventDefault();
-    const htmlData = '<div>非常抱歉：<br/>'
+    let node = document.createElement('div');
+    node.appendChild(window.getSelection().getRangeAt(0).cloneContents());
+    const htmlData = node.innerHTML
+        + '<div> ---------------------------<br/>'
         + '著作权归作者所有。<br/>'
         + '本作品采用知识共享署名-非商业性使用-禁止演绎 4.0 国际许可协议进行许可。<br/>'
-        + '您将不可以：转载，重新发布，商业使用!<br/>'
+        + '您将不可以：无出处转载，重新编辑发布，商业使用!<br/>'
         + '来源：青衫 Aspie<br/><br/>'
         + '</div>';
 
-    const textData = '非常抱歉：'
+    const textData =  window.getSelection().getRangeAt(0)
+        + '---------------------------'
         + '著作权归作者所有。'
         + '本作品采用知识共享署名-非商业性使用-禁止演绎 4.0 国际许可协议进行许可。'
-        + '您将不可以：转载，重新发布，商业使用！'
+        + '您将不可以：无出处转载，重新编辑发布，商业使用！'
         + '来源：青衫 Aspie';
 
     if (event.clipboardData) {
