a6fbcc3a5a40570c2f3919af39b9d2bc3d2fa219
740291272 <740291272@qq.com>
2020-02-29 10:41:35 +0800

    Fix duplicate definitions

---
+++ assets/js/QuotientsConst.js
@@ -0,0 +1,32 @@
+const ReTest = '<div class="row">' +
+    '<div class="col-md-12">' +
+    '<div class="submit_wrapper setres">' +
+    '<button class="btn btn-action btn-lg" onclick="ReTestFunc();">' +
+    '<span>重新测试&nbsp;</span><span class="fa fa-caret-right"></span>' +
+    '</button>' +
+    '</div>' +
+    '</div>' +
+    '</div>';
+
+
+const ADHD = {
+    "A": [{
+        "A": "您不太可能有ADHD"
+    }],
+    "B": [{
+        "A": "您很有可能有ADHD-I（注意力缺失为主）",
+        "B": "您非常有可能有ADHD-I（注意力缺失为主）"
+    }],
+    "C": [{
+        "A": "您很有可能有ADHD-H（过度运动为主）",
+        "B": "您非常有可能有ADHD-H（过度运动为主）"
+    }],
+    "D": [{
+        "A": "您很有可能有ADHD-C（不专心与过度运动混合）",
+        "B": "您非常有可能有ADHD-C（不专心与过度运动混合）"
+    }],
+    "E": [{
+        "A": "您很有可能有ADHD-C（不专心与过度运动混合，其中过度运动比较严重）",
+        "B": "您很有可能有ADHD-C（不专心与过度运动混合，其中注意力缺失比较严重）"
+    }]
+};
