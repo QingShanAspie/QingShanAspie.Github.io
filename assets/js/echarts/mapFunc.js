1982ffbddc7905571b3cafa1b64d06b7ed2b4a39
YuzukiTsuru <740291272@qq.com>
2020-09-22 22:44:31 +0800

    add visual Map

--- assets/js/echarts/mapFunc.js
+++ assets/js/echarts/mapFunc.js
@@ -13,14 +13,22 @@ let option = {
         backgroundColor: "#ffab91"
     },
     visualMap: {
-        show: false,
+        show: true,
+        type: 'piecewise',
         x: 'left',
         y: 'bottom',
         min: 0,
         max: 3,
+        splitNumber: 4,
         inRange: {
-            color: ['#e0f2f1', '#c8e6c9', '#f0f4c3', '#ffccbc']
-        }
+            color: ['#e0f2f1', '#c8e6c9', '#f0f4c3', '#ffccbc'],
+        },
+        pieces: [
+            {min: 0, max: 0.5, label: '暂无就诊信息'},
+            {min: 0.7, max: 1.3, label: '可诊断ASD'},
+            {min: 1.5, max: 2.3, label: '可诊断ADHD'},
+            {min: 2.5, max: 3.3, label: '可诊断ADHD与ASD'}
+        ],
     },
     series: [{
         type: 'map',
