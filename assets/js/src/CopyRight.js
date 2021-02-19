dcb161356e3bfb29f606ff12669d2ec0104c730a
柚木鉉 <740291272@qq.com>
2021-02-19 13:17:18 +0800

    add link to copy

--- assets/js/src/CopyRight.js
+++ assets/js/src/CopyRight.js
@@ -7,15 +7,17 @@ function setClipboardText(event) {
         + '著作权归作者所有。<br/>'
         + '本作品采用知识共享署名-非商业性使用-禁止演绎 4.0 国际许可协议进行许可。<br/>'
         + '您将不可以：无出处转载，重新编辑发布，商业使用!<br/>'
-        + '来源：青衫 Aspie<br/><br/>'
+        + 'Link: ' + window.location.href.toString() 
+        + '<br/>来源：青衫 Aspie'
         + '</div>';
 
-    const textData =  window.getSelection().getRangeAt(0)
+    const textData = window.getSelection().getRangeAt(0)
         + '\n\n---------------------------\n'
         + '著作权归作者所有。\n'
         + '本作品采用知识共享署名-非商业性使用-禁止演绎 4.0 国际许可协议进行许可。\n'
         + '您将不可以：无出处转载，重新编辑发布，商业使用！\n'
-        + '来源：青衫 Aspie';
+        + 'Link: ' + window.location.href.toString() 
+        + '\n来源：青衫 Aspie';
 
     if (event.clipboardData) {
         event.clipboardData.setData("text/html", htmlData);
