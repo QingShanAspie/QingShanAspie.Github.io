633658fc463a4cf55630220b51fb0d2b8347b812
YuzukiTsuru <gloomyghost@gloomyghost.com>
2023-07-10 13:28:50 +0800

    add china map and list

--- assets/js/echarts/map_data.js
+++ assets/js/echarts/map_data.js
@@ -4,24 +4,7 @@ function getInfo(name) {
             if (map_info[i].value == 0) {
                 return '<h7>暂无就诊信息<h7>';
             } else {
-                let map_data = '<h5>就诊地点：</h5><br>';
-                map_info[i].location.forEach(element => {
-                    map_data += element.name + '：<br>';
-                    element.doctor.forEach(ele => {
-                        map_data += ele.name;
-                        if (ele.name.search('科室') == -1) {
-                            map_data += ' 医生';
-                        }
-                        map_data += '（' + doc_clas(ele.clas) + '）';
-                        if (ele.desc != null) {
-                            map_data += '<small>' + ele.desc + '</small><br>';
-                        }
-                    });
-                    map_data += '<br>';
-                });
-                if (map_info[i].info_link != null) {
-                    map_data += '<br><h4>点击地图可查看就诊故事</h4>';
-                }
+                let map_data = '<h7>点击查看详细信息</h7>';
                 return map_data;
             }
         }
