fd5abd9214ae21a2b0170e45fff5c595fa5d58b7
柚木 鉉 <740291272@qq.com>
2024-01-07 15:48:44 +0800

    Update event_gray.js

--- assets/js/event_gray.js
+++ assets/js/event_gray.js
@@ -1,6 +1,7 @@
 let date = new Date();
 let event_grayDay = [ "1.11", "1.14", "1.15", "4.4", "9.18", "11.26", "12.13"]
 
+/*
 if (event_grayDay.indexOf((date.getMonth() + 1) + "." + (date.getDate())) !== -1) {
     $("html").css({
         "-webkit-filter": "grayscale(100%)",
@@ -11,3 +12,4 @@ if (event_grayDay.indexOf((date.getMonth() + 1) + "." + (date.getDate())) !== -1
         "_filter": "none"
     })
 }
+*/
