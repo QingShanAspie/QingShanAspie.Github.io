0b753211c3287e6723da1a83719e4b0238025248
YuzukiTsuru <gloomyghost@gloomyghost.com>
2024-04-02 15:33:35 +0800

    Add child map

--- assets/js/echarts/map_data.js
+++ assets/js/echarts/map_data.js
@@ -11,6 +11,18 @@ function getInfo(name) {
     }
 }
 
+function getInfo_child(name) {
+    for (let i = 0; i < map_info_child.length; ++i) {
+        if (map_info_child[i].name == name) {
+            if (map_info_child[i].value == 0) {
+                return '<h7>暂无就诊信息<h7>';
+            } else {
+                return '<h7>点击查看详细信息</h7>';
+            }
+        }
+    }
+}
+
 function doc_clas(level) {
     if (level == '1') {
         return '可确诊ASD';
